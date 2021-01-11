3ad0ab535bda524e461ebfc7a0429d840d307d59
GereyevKamran <jsfront20@gmail.com>
2021-01-11 09:38:19 +0400

    edit

--- src/components/film/cards.js
+++ src/components/film/cards.js
@@ -5,10 +5,13 @@ import "./film.css";
 function Cards(props) {
   return (
     <div className="notes">
-      <div className="notes__cards">
-        <p className="card__city">{props.filmName}</p>
-        <p className="card__countries">{props.raiting}</p>
-        <button className="remove__btn" onClick={() => props.onRemove(props.id)}>
+      <div className="notes__card">
+        <p className="card__film">{props.filmName}</p>
+        <p className="card__raiting">{props.raiting}</p>
+        <button
+          className="remove__btn"
+          onClick={() => props.onRemove(props.id)}
+        >
           X
         </button>
         <img className="card__image" src={props.images} />
